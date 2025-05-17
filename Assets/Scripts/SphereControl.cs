b7210c0585b90aa766bb14e961468ad3f678d22c
Ekaterina <burlutskayaekaterinka@yandex.ru>
2025-05-18 00:37:01 +0500

    refactor: change mouse controls

--- Assets/Scripts/SphereControl.cs
+++ Assets/Scripts/SphereControl.cs
@@ -2,26 +2,41 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
+using Unity.VisualScripting;
+
 using UnityEngine;
 
 public class SphereControl : MonoBehaviour
 {
     private Rigidbody _rigitbodySphere;
-    //private Transform _transformSphere;
     public float _speed = 5f;
+    public int _impulse = 300;
 
     private void Start() //Awake
     {
         _rigitbodySphere = GetComponent<Rigidbody>();
-        //_transformSphere = GetComponent<Transform>();
     }
 
     private void Update()
     {
-        //Цепляем объект к курсору мыши
+        if (Input.GetMouseButton(0))
+        {
+            FixItToMouse();
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                _rigitbodySphere.AddForce(0f, 0f, 5f);
+            }
+        }
+        else
+        {
+        }
+    }
+
+    private void FixItToMouse() //Цепляем объект к курсору мыши
+    {
         Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5f);
         Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
         transform.position = objPosition;
-        //конец
     }
 }
