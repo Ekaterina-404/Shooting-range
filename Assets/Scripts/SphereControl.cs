4f40a42b04d60cef5b4e159729f0db28875e8df8
Ekaterina <burlutskayaekaterinka@yandex.ru>
2025-06-08 16:29:11 +0500

    fix: end the CreateSphere method
    The transform component has been added

--- Assets/Scripts/SphereControl.cs
+++ Assets/Scripts/SphereControl.cs
@@ -8,23 +8,12 @@ using UnityEngine;
 
 public class SphereControl : MonoBehaviour
 {
+    private readonly float _distance = 5f;
     private Rigidbody _rigitbodySphere;
     private Transform _transformSphere;
-    private readonly float _distance = 5f;
-    [SerializeField] private GameObject _sphere;
-    //[SerializeField] private GameObject _spherePref;
     [SerializeField] private Material _material;
-    [SerializeField] private Quaternion _rotation = Quaternion.Euler(0,0,0);
     [SerializeField] private Vector3 _position;
 
-    private void CreateSphere ()
-    {
-        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        sphere.GetComponent<Renderer>().material = _material;
-        sphere.transform.position = _transformSphere.position;//позиция мыши
-        sphere.transform.rotation = _rotation;
-
-    }
 
     private void Start() //Awake
     {
@@ -40,13 +29,20 @@ public class SphereControl : MonoBehaviour
             FixItToMouse(_transformSphere);
         }
 
-        if (Input.GetMouseButtonUp(0)) //усиливается бросок, зависимость силы от времени, GetMouseButton
+        if (Input.GetMouseButtonUp(0))
         {
             _rigitbodySphere.useGravity = true;
-
             _rigitbodySphere.AddForce(Camera.main.transform.forward * 3000);
             CreateSphere();
         }
+    }
+    private void CreateSphere()
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.GetComponent<Renderer>().material = _material;
+        _transformSphere = sphere.GetComponent<Transform>();
+        _rigitbodySphere = sphere.AddComponent<Rigidbody>();
+        _rigitbodySphere.useGravity = false;
     }
 
     private void FixItToMouse(Transform transformSphere) //Цепляем объект к курсору мыши
@@ -55,6 +51,4 @@ public class SphereControl : MonoBehaviour
         Vector3 mousePositionInTheWorld = Camera.main.ScreenToWorldPoint(mousePosition);
         transformSphere.position = mousePositionInTheWorld;
     }
-    
-   //private void FixedUpdate()
 }
