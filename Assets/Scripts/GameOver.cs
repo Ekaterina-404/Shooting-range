41c262b28e11bf994d818a50f2d8397bb292b5c4
Ekaterina <burlutskayaekaterinka@yandex.ru>
2025-08-26 00:43:02 +0500

    Added a game completion sound

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ShootingRange
 {
     public class GameOver : MonoBehaviour
     {
         [SerializeField] private GameObject _gameOverUI;
+        [SerializeField] private AudioSource _audio;
+        [SerializeField] private Button _buttonRestart;
 
         public void EndTheGame()
         {
             _gameOverUI.SetActive(true);
+            _audio.Play();
+
         }
     }
 }
